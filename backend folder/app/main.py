0f093357dc1ffed719f0a2a7db7544f9723e238a
Tishan474 <tishanenghee@gmail.com>
2026-04-26 11:40:07 +0530

    Update main.py

--- backend folder/app/main.py
+++ backend folder/app/main.py
@@ -42,9 +42,11 @@ app = FastAPI(
 )
 
 # ── CORS ───────────────────────────────────────────────────────────────────────
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.origins_list,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
